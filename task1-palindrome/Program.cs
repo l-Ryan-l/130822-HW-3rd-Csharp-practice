bb3b8b3491a0bccba668e508d5dbd25953e3ed57
Ryan <llryanll@yandex.ru>
2022-08-18 22:04:41 +0300

    task1 solved

--- task1-palindrome/Program.cs
+++ task1-palindrome/Program.cs
@@ -3,3 +3,21 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+void Palindrome (string number)
+{
+  if (number[0] == number[4] && number[1] == number[3])
+  {
+    Console.WriteLine($"Ваше число: {number} - палиндром!");
+  }
+  else Console.WriteLine($"Ваше число: {number} - не явялется палиндромом.");
+}
+
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine()!;
+
+if (number.Length == 5)
+{
+  Palindrome(number);
+}
+else Console.WriteLine($"Введенное число не пятизначное. Попробуйте еще раз.");
